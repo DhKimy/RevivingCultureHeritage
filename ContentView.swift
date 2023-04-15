b67df37f6d9659baa9e01075f2dc37e1914c6af3
dhk767 <dhk767@gmail.com>
2023-04-15 23:00:58 +0900

    Remove the Useless Code

--- ContentView.swift
+++ ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     @GestureState private var isDetectingPress = false
     @State var openViewName : String = ""
     @State var isNavigationActive = false
+    @State private var tapped = false
     
     var body: some View {
         NavigationView {
@@ -19,17 +20,6 @@ struct ContentView: View {
                     ZStack {
                         Image(item.title)
                             .resizable()
-                        //                            .scaleEffect(isDetectingPress ? 1.5 : 1)
-                        //                            .animation(Animation.easeInOut(duration: 1.0))
-                        //                            .gesture(LongPressGesture(minimumDuration: 0.5).sequenced(before:DragGesture(minimumDistance: 0))
-                        //                                .updating($isDetectingPress) { value, state, _ in
-                        //                                    switch value {
-                        //                                    case .second(true, nil):
-                        //                                        state = true
-                        //                                    default:
-                        //                                        break
-                        //                                    }
-                        //                                })
                             .shadow(radius: 10, y: 30)
                             .gesture(getTapGesture(item.title))
                         Text(item.title)
@@ -37,56 +27,17 @@ struct ContentView: View {
                             .font(.system(size: 72, weight: .heavy, design: .serif))
                             .padding()
                             .foregroundColor(item.color)
-                        
-                        //                        Button(action: {
-                        //                            self.isNavigationActive = true
-                        //                            openViewName = item.title
-                        //
-                        //                        }) {
-                        //                            ZStack {
-                        //                                Image(item.title)
-                        //                                    .resizable()
-                        //                                Text(item.title)
-                        //                                    .fontWeight(.bold)
-                        //                                    .font(.system(size: 72, weight: .heavy, design: .serif))
-                        //                                    .padding()
-                        //                                    .foregroundColor(item.color)
-                        //
-                        //                            }
                     }
-                    //                        .gesture(getDragGesture())
-                    //
                     .frame(width: UIScreen.main.bounds.size.width / 1.9, height: UIScreen.main.bounds.size.height / 1.7)
                     
                     .scaleEffect(1.0 - abs(distance(item.id)) * 0.4 )
                     .opacity(1.0 - abs(distance(item.id)) * 0.5 )
                     .offset(x: myXOffset(item.id), y: 0)
                     .zIndex(1.0 - abs(distance(item.id)) * 0.1)
-                    
-                    //                .onTapGesture {
-                    //
-                    //                    // withAnimation is necessary
-                    //                    withAnimation {
-                    //                        draggingItem = Double(item.id)
-                    //                    }
-                    //                }
                 }
-                .tint(Color.black)
                 NavigationLink(destination: DetailView(title: openViewName), isActive: $isNavigationActive) {
-                    
                 }
-                
-                
-                
             }
-            
-            
-            //        .gesture(
-            //            TapGesture()
-            //                .onEnded { _ in
-            //                    print("Image was tapped")
-            //                })
-            
             .gesture(getDragGesture())
             .background(
                 Image("background")
@@ -100,26 +51,15 @@ struct ContentView: View {
         .navigationViewStyle(StackNavigationViewStyle())
     }
     
-    @State private var tapped = false
-    
-    var tap: some Gesture {
-        TapGesture(count: 1)
-            .onEnded{ _ in
-                
-            }
-    }
-    
     private func getTapGesture(_ title: String) -> some Gesture {
         TapGesture(count: 1)
             .onEnded { value in
-                print(tapped)
                 self.isNavigationActive = true
                 openViewName = title
             }
     }
     
     private func getDragGesture() -> some Gesture {
-        
         DragGesture()
             .onChanged { value in
                 draggingItem = snappedItem + value.translation.width / 100
